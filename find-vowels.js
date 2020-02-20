3c0f14975ea4a8038a1906023a6deeaacc521d2f
Shaonkabir <shaonkabir98@gmail.com>
2020-02-20 15:38:15 +0600

    Refactor(VowelFinder): count vowel numbers iterating a loop

--- find-vowels.js
+++ find-vowels.js
@@ -17,3 +17,31 @@ const countVowel = string => {
 
 
 countVowel("I am Shaon Kabir from Bangladesh") // Expected Output:- 10
+
+
+// Another Way:- using Loop
+
+// Process:-
+    // we'll store vowels in a container
+    // for caseSencetivity, we'll create string 'LowerCase'
+    // we'll convert our string into array removing whiteSpace to iterate a loop on it
+    // Then we'll Check using 'includes' methods if vowels available or not !
+    // If vowel found, we'll increase 'countVowel' in every iteration
+
+const findVowels = string => {
+    let countVowel = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u']; // or "aeiou"
+    
+    const convertedString = string.toLowerCase().split('').filter(space => /\S/.test(space)); 
+    console.log(convertedString)
+  
+    convertedString.forEach(character => {
+      if(vowels.includes(character)) {
+        countVowel++;
+      }
+    })
+  
+    return countVowel;
+  };
+  
+  findVowels("Hello Bangladesh !") // Expected Output:- 5
