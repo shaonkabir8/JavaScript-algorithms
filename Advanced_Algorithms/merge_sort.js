a7785bfe498c5cffc2ad868915b3f5d3835872e3
Shaonkabir <shaonkabir98@gmail.com>
2020-05-01 23:36:12 +0600

    feat: ✔ add Merge Sort algorithm

---
+++ Advanced_Algorithms/merge_sort.js
@@ -0,0 +1,16 @@
+/*
+    🔥 Algorithm Name: Merge Sort
+    ⚡ Reference: https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/implement-merge-sort
+*/
+
+
+// 🔥 Function_Description
+function mergeSort() {
+    // ⚡ Check if function is executing or not!
+    console.log('Execting from mergeSort')
+    
+
+}
+mergeSort()
+
+
